61c61ba0dd8a17f4dfe4cefb368905f2b9403147
kevortega <kevortega.dev@gmail.com>
2023-04-22 16:58:09 -0300

    add: next auth secret env

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const nextConfig = {
 		mongo_user: "kevortegadev",
 		mongo_password: "loo5z4n5wEp5PhcI",
 		NEXTAUTH_URL: "https://tyc-sports-login.vercel.app",
+		NEXTAUTH_SECRET: "6efeeb822a0c01b54a88adf05a6f01a7",
 		google_id:
 			"324768818465-3pkl2cq8dfiflv9bna73qtsbsffj5i5r.apps.googleusercontent.com",
 		google_secret_key: "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",
